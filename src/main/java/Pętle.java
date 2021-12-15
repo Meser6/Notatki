fb79a45cc24aaebbe89dbfdfedd8c89039bbf4f4
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2021-12-15 10:09:09 +0100

    7

---
+++ src/main/java/Pętle.java
@@ -0,0 +1,25 @@
+public class Pętle {
+    public static void main(String[] args) {
+
+        // While
+        int i = 1;
+
+        while (i <10){ // sprawdź czy YY. Jeśli prawda to zrób XX
+            System.out.println("cos");
+            i++;
+        }
+
+
+        // Do while
+        do{ // Zrób XX
+            System.out.println("cos");
+            i++;
+        } while (i >10); // a dopiero potem sprawdź YY. Jeśli prawda to zrob znowu i znowu sprawdź.
+
+
+        // For
+        for (int j = 0; j <10; j++){ // przyjmij j. sprawdz czy j<10. jeśl tak to wykonaj XX a potem zrob j++
+            System.out.println("cos");
+        }
+    }
+}
