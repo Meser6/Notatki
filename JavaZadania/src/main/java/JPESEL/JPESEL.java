3d69e5a53c89c3a9ecd3417a6a31be209f24bb71
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2022-01-24 10:53:27 +0100

    JPESEL

--- JavaZadania/src/main/java/JPESEL/JPESEL.java
+++ JavaZadania/src/main/java/JPESEL/JPESEL.java
@@ -6,36 +6,19 @@ import java.util.Stack;
 
 public class JPESEL {
     /*
-    https://pl.spoj.com/problems/JPESEL/
+    https://pl.spoj.com/problems/JPESEL/    (zmodyfikowana bo podana tam metoda byla zla)
 
 Jan Kowalski musi wpisać do systemu szpitalnego dane osobowe pacjenta, oprócz imienia i nazwiska musi również wpisać
 PESEL pacjenta. Jakież było jego zdziwienie, gdy spostrzegł, że pewnych pacjentów system nie przyjmował z powodu wadliwego PESELu.
 
 Twoim zadaniem jest sprawdzenie, czy podana liczba 11-cyfrowa jest poprawnym PESELem.
 
-Aby sprawdzić czy dany PESEL jest prawidłowy należy wykonać następujące działania:
+Ostatnia cyfra numeru, to tzw. liczba kontrolna, która pozwala obliczyć czy numer PESEL został ustalony prawidłowo. Obliczenia tego dokonuje się według prostego wzoru matematycznego:
 
-Pierwszą cyfrę mnożymy przez 1,
-drugą cyfrę mnożymy przez 3,
-trzecią cyfrę mnożymy przez 7,
-czwarta cyfrę mnożymy przez 9,
-piątą cyfrę mnożymy przez 1,
-szóstą cyfrę mnożymy przez 3,
-siódmą cyfrę mnożymy przez 7,
-ósmą cyfrę mnożymy przez 9,
-dziewiątą cyfrę mnożymy przez 1,
-dziesiątą cyfrę mnożymy przez 3,
-jedenastą cyfrę mnożymy przez 1.
+1*a + 3*b + 7*c + 9*d + 1*e + 3*f + 7*g + 9*h + 1*i + 3*j, gdzie litery od a do j oznaczają kolejne cyfry numeru PESEL.
+Uwaga chodzi o 10 z 11 cyfr numeru. Ostatnia cyfra PESEL będzie wynikiem tego wzoru.
 
-Tak uzyskane 11 iloczynów dodajemy do siebie. Jeśli ostatnia cyfra tej sumy jest zerem to podany PESEL jest prawidłowy. Przykład dla numeru PESEL 44051401458
-
-4*1 + 4*3 + 0*7 + 5*9 + 1*1 + 4*3 + 0*7 + 1*9 + 4*1 + 5*3 + 8*1 = 4 + 12 + 0 + 45 + 1 + 12 + 0 + 9 + 4 + 15 + 8 = 110
-
-Źródło: www.wikipedia.pl
-
-Jeśli suma jest większa od zera, wtedy sprawdzamy jej poprawność. W przeciwnym przypadku nr PESEL jest błędny. Ponieważ ostatnia cyfra liczby 110 jest zerem więc podany PESEL jest prawidłowy.
-
-Na wejściu podana jest w pojedyńczej linii ilość t<=100 numerów PESEL do sprawdzenia. W kolejnych t liniach są 11-cyfrowe liczby.
+Z otrzymanego wyniku bierze się ostatnią cyfrę i odejmuje się od liczby 10. Tak uzyskany wynik powinien być zgodny z naszą ostatnią cyfra numeru PESEL.
 
 Output
 W pojedyńczej linii powinna zostać wyświetlona litera D, jeśli numer PESEL jest poprawny lub N, gdy nie.
@@ -46,7 +29,7 @@ W pojedyńczej linii powinna zostać wyświetlona litera D, jeśli numer PESEL j
         int quantity = 0;
         System.out.println("Jak dużo PESELi chcesz sprawdzić?");
         try {
-            quantity = 1; //scanner.nextInt();
+            quantity = scanner.nextInt();
         } catch (Exception e) {
             System.out.println("Podano zła wartość");
         }
@@ -57,7 +40,7 @@ W pojedyńczej linii powinna zostać wyświetlona litera D, jeśli numer PESEL j
             String string = String.valueOf(pesel);
             if (string.length() < 11) {
                 System.out.println("N");
-            } else if (isCorect(pesel)) {
+            } else if (10 - lastNumber(sum10(pesel)) == lastNumber(pesel)) {
                 System.out.println("D");
             } else System.out.println("N");
         }
@@ -68,14 +51,14 @@ W pojedyńczej linii powinna zostać wyświetlona litera D, jeśli numer PESEL j
         ArrayList<Long> peselsList = new ArrayList<>();
         for (int i = 0; i < quantity; i++) {
             System.out.print("Podaj PESEL:");
-            long pesel = 95042110237L;//scanner.nextLong();
+            long pesel = scanner.nextLong();
             peselsList.add(pesel);
         }
         return peselsList;
     }
 
 
-    static boolean isCorect(long pesel) {
+    static int sum10(long pesel) {
         final int[] multiper = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
         int sum = 0;
         int[] numbers = list(pesel);
@@ -83,12 +66,22 @@ W pojedyńczej linii powinna zostać wyświetlona litera D, jeśli numer PESEL j
         for (int i = 0; i < 10; i++) {
             sum += numbers[i] * multiper[i];
         }
-        int check = 10 - lastNumber(sum);
-        //TODO ostatnia liczba powinna  taka jak 10-ostatnia cyfra sumy z 10 liczb
-        return check == numbers[10];
+        return sum;
     }
 
     static int lastNumber(int suma) {
+        Stack<Integer> list = new Stack<>();
+        String peselString = String.valueOf(suma);
+
+        for (int i = 0; i < peselString.length(); i++) {
+            char znak = peselString.charAt(i);
+            int cyfra = Character.getNumericValue(znak);
+            list.push(cyfra);
+        }
+        return list.lastElement();
+    }
+
+    static int lastNumber(long suma) {
         Stack<Integer> list = new Stack<>();
         String peselString = String.valueOf(suma);
 
