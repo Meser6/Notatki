d9663c4eee65df86cef9c484edd973e2d3ed2ce7
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2022-01-27 23:23:41 +0100

    com

--- JavaZadania/src/main/java/DOSTEP/DOSTEP.java
+++ JavaZadania/src/main/java/DOSTEP/DOSTEP.java
@@ -3,5 +3,14 @@ package DOSTEP;
 public class DOSTEP {
     /*
     https://www.samouczekprogramisty.pl/modyfikatory-dostepu-w-jezyku-java/
+
+    Napisz typ wyliczeniowy Computation, który będzie reprezentował prosty kalkulator. Niech typ ten posiada następujące
+     wartości MULTIPY, DIVIDE, ADD, SUBTRACT. Niech typ ten posiada metodę public double perform(double x, double y),
+     która zwróci wynik odpowiedniej operacji. Na przykład Computation.ADD.perform(1, -5) powinno zwrócić -4.
      */
+
+    public static void main(String[] args) {
+
+
+    }
 }
---
+++ JavaZadania/src/main/java/POROWNANIE/POROWNANIE.java
@@ -0,0 +1,12 @@
+package POROWNANIE;
+
+public class POROWNANIE {
+    /*
+    https://www.samouczekprogramisty.pl/porownywanie-obiektow-metody-equals-i-hashcode-w-jezyku-java/
+
+    Na koniec krótkie zadanie dla Ciebie. Napisz klasę reprezentującą człowieka, zaimplementuj metody
+    hashCode i equals. Zastanów się czy to, że ktoś ma to samo imię i nazwisko sprawia, że jest to
+    ta sama osoba? Jaki atrybut może posłużyć do sprawdzenia czy dwie instancje klasy Human
+    reprezentują tę samą osobę?
+     */
+}
