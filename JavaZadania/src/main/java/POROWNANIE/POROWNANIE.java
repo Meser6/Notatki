379201889c7696f0c90037e358157c94f2fcfaaa
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2022-01-28 13:24:01 +0100

    POROWNANIE

--- JavaZadania/src/main/java/POROWNANIE/POROWNANIE.java
+++ JavaZadania/src/main/java/POROWNANIE/POROWNANIE.java
@@ -9,4 +9,11 @@ public class POROWNANIE {
     ta sama osoba? Jaki atrybut może posłużyć do sprawdzenia czy dwie instancje klasy Human
     reprezentują tę samą osobę?
      */
+
+    public static void main(String[] args) {
+        Human h1 = new Human("Kuba", "Kaczmarczyk", 95042110237L);
+        Human h2 = new Human("Kuba", "Kaczmarczyk", 72031332339L);
+
+        System.out.println(h1.equals(h2));
+    }
 }
