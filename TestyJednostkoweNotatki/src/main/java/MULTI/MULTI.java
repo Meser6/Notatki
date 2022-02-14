b25d86c63a9834b79d8ff2315f3b375bf5c96957
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2022-02-14 15:49:52 +0100

    sub testy jednostkowe

---
+++ TestyJednostkoweNotatki/src/main/java/MULTI/MULTI.java
@@ -0,0 +1,23 @@
+package MULTI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MULTI {
+
+   private List<Integer> list = new ArrayList<>();
+
+    void addNumberToList(int number){
+        list.add(number);
+    }
+
+    void cleanList(){
+        list.clear();
+    }
+
+    public List<Integer> getList() {
+        return list;
+    }
+
+
+}
---
+++ TestyJednostkoweNotatki/src/main/java/TIMEOUT/TIMEOUT.java
@@ -0,0 +1,27 @@
+package TIMEOUT;
+
+import java.util.Random;
+
+public class TIMEOUT {
+
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setRandomA(){
+        Random random = new Random();
+        a = random.nextInt(1000);
+    }
+
+    public void simulate(int p){
+        for (int i = 0; i < p; i++ ){
+            setRandomA();
+        }
+    }
+}
