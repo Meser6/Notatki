a054f66b256c2f815c8f082c3c0ddc15e867458a
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2022-03-08 17:33:56 +0100

    Selenium

--- SeleniumNotatki/src/test/java/Selenium.java
+++ SeleniumNotatki/src/test/java/Selenium.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,13 +9,19 @@ import java.net.URL;
 
 public class Selenium {
 
+    WebDriver driver;
+
     @Test
-    void Selenium() throws MalformedURLException {
+    void przygotowanie() {
         //Podanie ścieżki do ChromeDriver
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
         //Inicjalizacja drivera
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void nawigacjaPoStonie() throws MalformedURLException {
 
         //Wejście na daną strone
         URL google = new URL("https://google.pl");
@@ -36,7 +44,10 @@ public class Selenium {
 
         //Zamknięcie okna przeglądarki bez zamykania procesu
         driver.close();
+    }
 
+    @Test
+    void pobranieInfo() {
         //Pobranie aktualnego adresu strony
         driver.getCurrentUrl();
 
@@ -45,6 +56,30 @@ public class Selenium {
 
         //Pobranie całego "kodu" strony
         driver.getPageSource();
+    }
 
+    @Test
+    void obsługaOknaPrzeglądarki() {
+        //Wyświetlenie okna na pełnym ekranie
+        driver.manage().window().fullscreen();
+
+        //Maksymalizacja okna na cały ekran
+        driver.manage().window().maximize();
+
+        //Ustawienie pozycji okna (w odniesieniu do lewego górnego rogu ekranu)
+        Point point = new Point(10, 12);
+        driver.manage().window().setPosition(point);
+
+        //Ustawienie rozmiaru okna
+        Dimension dimension = new Dimension(100, 123);
+        driver.manage().window().setSize(dimension);
+
+        //Pobranie pozycji okna (w odniesieniu do lewego górnego rogu ekranu)
+        driver.manage().window().getPosition();
+
+        //Pobranie rozmiaru okna
+        driver.manage().window().getSize();
+
+        //
     }
 }
