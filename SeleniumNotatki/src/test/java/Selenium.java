ed807c911c91c5d6e8dd3a430037898de4aa4632
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2022-04-11 12:23:18 +0200

    grid/webdrivery

--- SeleniumNotatki/src/test/java/Selenium.java
+++ SeleniumNotatki/src/test/java/Selenium.java
@@ -1,8 +1,17 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,10 +22,36 @@ public class Selenium {
 
     @Test
     void przygotowanie() {
-        //Podanie ścieżki do ChromeDriver
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        //Chrome
+        System.setProperty("webdriver.chrome.driver", "ścieżka_do_pliku_drivera");
+        driver = new ChromeDriver();
 
-        //Inicjalizacja drivera
+        //Firefox
+        System.setProperty("webdriver.gecko.driver", "ścieżka_do_pliku_drivera");
+        driver = new FirefoxDriver();
+
+        //Opera
+        System.setProperty("webdriver.opera.driver", "ścieżka_do_pliku_drivera");
+        OperaOptions operaOptions = new OperaOptions();
+        operaOptions.setBinary("ścieżka_do_pliku_opera.exe");
+        driver = new OperaDriver(operaOptions);
+
+        //Internet exploder
+        //żeby zadziałało driver.quit() trzeba wejsc w Opcje > Opcje internetowe > Zabezpieczenia > Odklikać Tryb chroniony w każdej z opcji
+        System.setProperty("webdriver.ie.driver", "ścieżka_do_pliku_drivera");
+        driver = new InternetExplorerDriver();
+
+        //EDGE do v.18
+        System.setProperty("webdriver.edge.driver", "ścieżka_do_pliku_drivera");
+        driver = new EdgeDriver();
+
+        //EDGE od v.18
+        //Wejdz w Ustawienia > Aplikacje > Funkcje opcjonalne > Dodaj funkcje > Sterownik sieci Web firmy Microsoft
+        driver = new EdgeDriver();
+
+        //Driver manager - bardzo przydatna funcja. Zczyta jaka sie ma przegladarke i samo sciagnie odpowiedniego drivera - dziala tylko lokalnie
+        //https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
 
@@ -79,7 +114,32 @@ public class Selenium {
 
         //Pobranie rozmiaru okna
         driver.manage().window().getSize();
-
-        //
     }
+
+    @Test
+    void seleniumGrid() throws MalformedURLException {
+        //Selenium grid służy do pusczania testów w trybie rosproszonym
+
+        /*
+        Polega to na tym, ze jest postawiony jeden hub który ma pod soba kilka node.
+        Kazdy z node ma swoje parametry (przegladarki i ich wersje, wersje systemow itp)
+        Za pomocą grida mozna tak skonfigurowac zeby wybrac na maszynie z jakimi parametrami je puscic
+         */
+
+
+        //Wysyłamy testy do Huba a on nam znajdzie node ktory będzie miał zadane parametry.
+        //Jeśli nie znajdzie node z takimi parametrami to rzuci wyjatkiem
+        //Jeśli nie podamy konkretnych parametrów to znajdzie dowolne
+
+        //Docelowo szuka po 3 parametrach:
+        ChromeOptions options = new ChromeOptions(); // nazwa strony. Nie potrzeba podawać bo domysli sie z typu opcji
+        options.setCapability(CapabilityType.BROWSER_VERSION, "101"); // wersja przeglądarki
+        options.setCapability(CapabilityType.PLATFORM_NAME, "WIN10"); // wersja systemu operacyjnego maszyny
+
+        //Jeżeli skonfigutrowalismy node tak ze ma dodatkowe parametry to tez mozemy z nich skorzystac
+        options.setCapability("nazwa_parametru", "wartość_parametru");
+
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+    }
+
 }
