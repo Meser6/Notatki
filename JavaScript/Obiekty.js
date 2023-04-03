c02e1d1acbd78780ec5a58e3ce8df4fb5c8cd2fd
Meser6 <j.kaczmarczyk95@gmail.com>
2023-04-03 18:40:59 +0200

    js

--- JavaScript/Obiekty.js
+++ JavaScript/Obiekty.js
@@ -1,74 +1,115 @@
-// obiekt to struktura danych skladajaca sie z par kluczy i wartosci 
+// obiekt to struktura danych skladajaca sie z par kluczy i wartosci
 const objekt = {
-    klucz: "wartosc",
-    klucz2: 15-6, //wartoscia moze byc dowolna rzecz zwracajaca dane
-    klucz3: [1, 2, 3],
-    klucz4: value => value  -9 
-    //kolejnosc nie ma znaczenia bo i tak zostanie ulozona w kolejnosci alfabetycznej
-}
+  klucz: "wartosc",
+  klucz2: 15 - 6, //wartoscia moze byc dowolna rzecz zwracajaca dane
+  klucz3: [1, 2, 3],
+  klucz4: (value) => value - 9,
+  jakasFunkcja(text) {
+    // mozemy tez przetrzymywac w niej funckcje
+    console.log(`xd ${text}`);
+  },
+  //kolejnosc nie ma znaczenia bo i tak zostanie ulozona w kolejnosci alfabetycznej
+};
+
+objekt.jakasFunkcja("xd"); // do wlasciwosci obiektow odnosimy sie przez kropke
+objekt["klucz"]; // lub poprzez wrzucenie stringa do []
 
 //Obiekty można stworzyć na dwa sposoby:
 //1 bez konstruktora (przydatne jak chcemy szybko stworzyć jeden obiekt i nie tworzyć  podobnych)
-const adam= {
-    imie: "Adam", 
-    wiek: 12
+const adam = {
+  imie: "Adam",
+  wiek: 12,
 };
 
 //2 z konstruktorem
-function Czlowiek(imie, wiek){ //konstruktor nazywamy z duzej litery
-    this.imie = imie; // tutaj przypisujemy wysylany argument do zmiennej w konstruktorze
-    this.wiek = wiek; // słówko this nie jako tworzy nowa zmienna do ktorej nastepnie przypisujemy argument
-}
-
-// this wskasuje na obiekt w ktorym je wywolamy
-const thisObjekt = {
-    numner: 15,
-    number2: this.numner - 1, // zadziala tak samo jakbysmy napisali thisObjekt.numer 
-    objekt: this // w ten sposob zwroci caly objekt 
+function Czlowiek(imie, wiek) {
+  //konstruktor nazywamy z duzej litery
+  this.imie = imie; // tutaj przypisujemy wysylany argument do zmiennej w konstruktorze
+  this.wiek = wiek; // słówko this nie jako tworzy nowa zmienna do ktorej nastepnie przypisujemy argument
 }
 
 const beata = new Czlowiek("Beata", 12);
-const krystian =  new Czlowiek("Krystian"); // gdy nie wypełnimy wszytkich argumentów to obiekt i tak powstanie
+const krystian = new Czlowiek("Krystian"); // gdy nie wypełnimy wszytkich argumentów to obiekt i tak powstanie
 //a puste argumenty przyjmia pustą wartość
+
+// this wskasuje na obiekt w ktorym je wywolamy
+const thisObjekt = {
+  numner: 15,
+  number2: this.numner - 1, // zadziala tak samo jakbysmy napisali thisObjekt.numer
+  objekt: this, // w ten sposob zwroci caly objekt
+};
+
+//skrocony zapis
+//jesli nazwa zmiennej jest taka sama jak nazwa wartosci to mozemy zastosowac skrocony zapis
+const names = "Szama";
+const speed = 1000;
+
+const dog = {
+  names,
+  speed,
+  showText() {
+    return "Lubię walczyć ze złem";
+  },
+};
 
 //POBIERANIE - aby dostac sie do parametrow danego obiektu nalezy go wywolac i dodac kropke
 beata.imie;
 beata["imie"]; //alternatywny sposob
-beata[nazwa()] // dowonna ekspresje mozemu tu wrzucic
-beata.nieistnieje // jak odwolamy sie do nieistniejacej wartosci to dostaniemy undefined 
+beata[nazwa()]; // dowonna ekspresje mozemu tu wrzucic
+beata.nieistnieje; // jak odwolamy sie do nieistniejacej wartosci to dostaniemy undefined
 
-const nazwa = () => "imie"
-
-beata.nowyKlucz = 'to jest nowa wartosc' // mozemy dodawac do obiektu nowe pary, ale nie mozemy dodac nic bez wartosci
-beata.imie = "beti" // mozemy tez podmieniac wartosci
+const nazwa = () => "imie";
+//DODAWANIE
+beata.nowyKlucz = "to jest nowa wartosc"; // mozemy dodawac do obiektu nowe pary, ale nie mozemy dodac nic bez wartosci
+beata.imie = "beti"; // mozemy tez podmieniac wartosci
+//USUWANIE
+//jesli chcemy usunac jakis klucz i waetosc z obiektu to robimy to przez delete.
+delete beata.imie;
 
 //tworzenie metod w obiektach - implementujemu je w ciele obiektu/konstruktrze
 //w przypadku 1 sposobu
 const kuba = {
-    imie: "Kuba", 
-    wiek: 15,
-    przedstawSie(){ // krótsza forma
-        return `mam na imie ${this.imie}`; // moze cos zwracac
-    },
-    podajWiek: function(){ //dłuzsza forma
-        console.log(`mam ${wiek} lat`) // ale nie musi
-    }
-}
+  imie: "Kuba",
+  wiek: 15,
+  przedstawSie() {
+    // krótsza forma
+    return `mam na imie ${this.imie}`; // moze cos zwracac
+  },
+  podajWiek: function () {
+    //dłuzsza forma
+    console.log(`mam ${wiek} lat`); // ale nie musi
+  },
+};
 
 kuba.przedstawSie();
 
 //w przypadku 2 sposobu
-function Pies (imie, rasa){
-    this.imie = imie;
-    this.rasa = rasa;
-    this.przedstawSie = function(){
-            return `mam na imie ${imie} i jestem ${rasa}`;
-    },
-    this.zaszczekaj = function(szczek){ // oczywiscie mozna tworzyc metody z argumentami i nic nie zwracajace
-        console.log(`${imie} szczeka ${szczek}`);
-    }
+function Pies(imie, rasa) {
+  this.imie = imie;
+  this.rasa = rasa;
+  (this.przedstawSie = function () {
+    return `mam na imie ${imie} i jestem ${rasa}`;
+  }),
+    (this.zaszczekaj = function (szczek) {
+      // oczywiscie mozna tworzyc metody z argumentami i nic nie zwracajace
+      console.log(`${imie} szczeka ${szczek}`);
+    });
 }
 
-const azor = new Pies("azor", "mieszaniec")
+const azor = new Pies("azor", "mieszaniec");
 azor.zaszczekaj("how how");
-azor['zaszczekaj']('how how')
+azor["zaszczekaj"]("how how");
+
+// duplikowanie obiektu
+//mamy tablicę która chcemy posortować
+const tab = [4, 10, 2, 11, 1.1, 3]; // tabela to tak na prawde obiekt
+
+const sortNumbers = function (arr) {
+  return arr.sort((a, b) => a - b); // funkcja ta pobierze nam tabele, posortue i przyisze do zmiennej
+}; // z tym ze tak na prawde funkcja ta nie stworzy nam nowej tabeli a dopiszse tylko referenche starej
+
+const tabSorted = sortNumbers(tab);
+console.log(tabSorted); //[1.1, 2, 3, 4, 10, 11]
+console.log(tab); //[1.1, 2, 3, 4, 10, 11]
+
+// https://kursjs.pl/kurs/obiekty/obiekty-duplikowanie
--- JavaScript/Pętle.js
+++ JavaScript/Pętle.js
@@ -1,29 +1,40 @@
 // While
 let i = 1;
 
-while (i < 10) { // sprawdź czy YY. Jeśli prawda to zrób XX i znowu sprawdź.
-    console.log(i);
-    i++;
+while (i < 10) {
+  // sprawdź czy YY. Jeśli prawda to zrób XX i znowu sprawdź.
+  console.log(i);
+  i++;
 }
 
 // Do while
-do { // Zrób XX
-    console.log(i);
-    i++;
+do {
+  // Zrób XX
+  console.log(i);
+  i++;
 } while (i > 10); // a dopiero potem sprawdź YY. Jeśli prawda to zrob znowu i znowu sprawdź.
 
 // For
-for (let j = 0; j < 10; j++) { // przyjmij j. sprawdz czy j<10. jeśl tak to wykonaj XX a potem zrob j++
-    console.log(j);
+for (let j = 0; j < 10; j++) {
+  // przyjmij j. sprawdz czy j<10. jeśl tak to wykonaj XX a potem zrob j++
+  console.log(j);
 }
 
 // ForEach
-const tab = new Array(1,2,3);
-tab.forEach(x => console.log(x)); // bierze element, wpisuje go w x, potem robi to co po strzlce i bierze kolejny itd.
+const tab = new Array(1, 2, 3);
+tab.forEach((x) => console.log(x)); // bierze element, wpisuje go w x, potem robi to co po strzlce i bierze kolejny itd.
 
-//innym zapisem literowania po elementach jest for of 
-for(const x of tab){
-    //dopisze kazdy element tablicy tab do x i cos z nim zrobi
+//innym zapisem literowania po elementach jest for of
+for (const x of tab) {
+  //dopisze kazdy element tablicy tab do x i cos z nim zrobi
+}
+
+//iterowanie po kluczach obiektu
+for (const key in car) {
+  if (car.hasOwnProperty(key)) {
+    // hasOwnProperty sluzy do upewnoenia sie ze obiekt ma ten klucz
+    console.log(key);
+  }
 }
 
 //--------- Etykiety ----------------
@@ -31,61 +42,55 @@ for(const x of tab){
 //Dane fragmenty kodu mozna etykietowac a potem je wskazywać
 //robi się to poprzez nadamie imienia i dodanie :
 
-etykieta:
-{
-tekst.innerHTML += "test <br>"
+etykieta: {
+  tekst.innerHTML += "test <br>";
 }
 
 //--------- Instrukcje skoku -------------
 
 //continue - przeskakuje do kolejnej iteracji pętli ignorując w obecnej iteracji to co jest pod nim
-for(let i = 0; i < 4; i++ ){
-    if(i == 2){
-        continue; // nie wypisze 2
-    }
-    tekst.innerHTML += i + "<br>"
+for (let i = 0; i < 4; i++) {
+  if (i == 2) {
+    continue; // nie wypisze 2
+  }
+  tekst.innerHTML += i + "<br>";
 }
 
 //pętle można etykietować i wskazac która pętla ma byc kontynuowana
-pierwsza:
-for(let i = 0; i < 5; i++ ){
-    tekst.innerHTML += "i: " + i + "<br>"
-    druga:
-    for(let j = 10; j >0 ; j--){
-       if (j == 4){
-            continue pierwsza; // jeżeli j == 4 to opusci pętlę druga i wejdzie w kolejna iteracje pierwszej
-       }
-       tekst.innerHTML += "j: " + j + "<br>"
+pierwsza: for (let i = 0; i < 5; i++) {
+  tekst.innerHTML += "i: " + i + "<br>";
+  druga: for (let j = 10; j > 0; j--) {
+    if (j == 4) {
+      continue pierwsza; // jeżeli j == 4 to opusci pętlę druga i wejdzie w kolejna iteracje pierwszej
     }
+    tekst.innerHTML += "j: " + j + "<br>";
+  }
 }
 
 //break -- po natknięciu się na niego zamyka cala pętlę na obecnej iteracjii nie wykonuje kolejnych
-for(let i = 0; i <10; i++){
-    tekst.innerHTML += i + "<br>"
-    if (i == 7){
-        break;
-    }
+for (let i = 0; i < 10; i++) {
+  tekst.innerHTML += i + "<br>";
+  if (i == 7) {
+    break;
+  }
 }
 
 //etykiet mozna uzyc rowniez brzy brake zeby wybrac ktora petla ma sie zakonczyc
-pierwsza:
-for(let i = 0; i < 5; i++ ){
-    tekst.innerHTML += "i: " + i + "<br>"
-    druga:
-    for(let j = 10; j >0 ; j--){
-       if (j == 4){
-            break pierwsza;
-       }
-       tekst.innerHTML += "j: " + j + "<br>"
+pierwsza: for (let i = 0; i < 5; i++) {
+  tekst.innerHTML += "i: " + i + "<br>";
+  druga: for (let j = 10; j > 0; j--) {
+    if (j == 4) {
+      break pierwsza;
     }
+    tekst.innerHTML += "j: " + j + "<br>";
+  }
 }
 
 //break  i continue mozna uzywac nie tylko w pętlach a calych etykietach
 
-etykieta2:
-{
-    alert("pierwszy");
-    break etykieta2; // tak na prawde w tym przypadku nie trzeba podawac nazwy bo bez niej zawsze sie dotyczy etykiety/
-    // / petli w ktorej jest
-    alert("drugi"); // ten juz sie nie wykona
+etykieta2: {
+  alert("pierwszy");
+  break etykieta2; // tak na prawde w tym przypadku nie trzeba podawac nazwy bo bez niej zawsze sie dotyczy etykiety/
+  // / petli w ktorej jest
+  alert("drugi"); // ten juz sie nie wykona
 }
