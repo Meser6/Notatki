ebf172a171189365f8adc68a8689ec19f96b0ba0
Meser6 <j.kaczmarczyk95@gmail.com>
2023-04-08 16:49:35 +0200

    js dom

--- JavaScript/DOM.js
+++ JavaScript/DOM.js
@@ -1,31 +1,29 @@
 document; //pobiera cały DOM
 
 //pobierajac element pobieramy jego i jego dzieci.
-const id = document.getElementById("ID"); // pobiera element po ID (powinno byc unikalne)
+const id = document.getElementByelement("element"); // pobiera element po element (powinno byc unikalne)
 const classes = document.getElementsByClassName("CLASS"); // pobiera elementy o danej klasie i dopisuje je do listy
 const tags = document.getElementsByTagName("TAG"); // pobiera elementy o danym tagu i dopisuje je do listy
 const names = document.getElementsByName("NAME"); // pobiera elementy o danej nazwie i dopisuje je do listy
-const selector = document.querySelector("CSS_SELECTOR") // pobiera pierwszy element z danym seletorem css
-const selectors = document.querySelectorAll("CSS_SELECTOR") // pobiera wszystkie elementy z danym selektorem i opisuje do listy
+const selector = document.querySelector("CSS_SELECTOR"); // pobiera pierwszy element z danym seletorem css
+const selectors = document.querySelectorAll("CSS_SELECTOR"); // pobiera wszystkie elementy z danym selektorem i opisuje do listy
 
-
-id.innerText = "<br>TEXT</br>"; // podmieni cały teskt elementu i NIE będzie szukał w nim znaczników HTML (wstawi "<br>TEXT</br>")
-id.innerHTML = "<br>TEXT</br>"; // podmieni cały teskt elementu i BĘDZIE szukał w nim znaczników HTML (wstawi "TEXT" pogrubione)
 id.parentNode; // pobierze rodzica elementu
+
 /*
-1   <ul id="list">
-2       <li id="li1">jeden</li>
-3       <li id="li2">dwa</li>
-4       <li id="li3">trzy</li>
+1   <ul element="list">
+2       <li element="li1">jeden</li>
+3       <li element="li2">dwa</li>
+4       <li element="li3">trzy</li>
 5   </ul>
 */
 
-const lista = document.getElementById("list");
+const lista = document.getElementByelement("list");
 
-lista.firstElementChild // wyszuka pierwsze dziecko elementu (2)
-lista.lastElementChild // wyszuka ostatnie dziecko elementu (3)
+lista.firstElementChild; // wyszuka pierwsze dziecko elementu (2)
+lista.lastElementChild; // wyszuka ostatnie dziecko elementu (3)
 
-const li2 = document.getElementById("dwa");
+const li2 = document.getElementByelement("dwa");
 //dodanie nowego elementu
 
 const newLi = document.createElement("li"); //tworzy w dokumencie nowy element o podanym tagu (ale nigdzie nie dodaje)
@@ -36,10 +34,32 @@ lista.appendChild(newLi); // dodaje li jako dziecko do elementu (4-5)
 lista.insertBefore(newLi, li2); // dodaje li jako dziecko do elelemtu przed elementem (dodawany, przed czym) (2-3)
 
 lista.appendChild(li2.cloneNode()); // klonuje i dodaje do elementu (lista) dany (li2) element (ale bez jego dzieci!)
-lista.appendChild(li.cloneNode(true));// klonuje i dodaje do elementu (lista) dany (li2) element (z jego dziećmi!)
+lista.appendChild(li.cloneNode(true)); // klonuje i dodaje do elementu (lista) dany (li2) element (z jego dziećmi!)
 
 //zamiana istniejacego elementu
 lista.replaceChild(newLi, li2); // zamienia element z drugim (nowy, stary) (3)
 
 //usuniecie danego elementu
 lista.removeChild(li2);
+
+//MANIPULACJA DOMem
+//dane wlasciwosci obiektu dom (jak teskt) sa jego wliwosciami sensu stricte wiec aby je zmienic musimy
+//odwolac sie jak do wlasciwosci elementu
+const element = document.querySelector("selektor");
+element.textContent = "nowy tekst";
+element.textContent("to nie zadziala bo to nie metoda!!!");
+
+//tekst
+element.innerText = "<br>TEXT</br>"; // podmieni cały teskt elementu i NIE będzie szukał w nim znaczników HTML (wstawi "<br>TEXT</br>")
+element.innerHTML = "<br>TEXT</br>"; // podmieni cały teskt elementu i BĘDZIE szukał w nim znaczników HTML (wstawi "TEXT" pogrubione)
+element.textContent; // dostep do tekstu elementu
+document.querySelector("input").value; // dostep do wartosci wpisanej w inpucie. zwroci stringa. jak bedzie puste to zwroci 0
+
+//EVENTY
+//event listner sluzy do nasluchiwania jakichs akcji na danym elemencie i wykonywania instrukcji jesli one wystapia
+
+element.addEventListener("click", () => console.log("cos zrobi"));
+// przyjmuje          typ eventu,  funkcje ktora wykona sie gdy ten event nastapi
+
+//typy eventow:
+const typyEventow = ["click"];
--- JavaScript/OperatoryArytmetyczne.js
+++ JavaScript/OperatoryArytmetyczne.js
@@ -1,40 +1,42 @@
 //operatoty atytmetyczne sa takie same jak w Java z niewielkimi roznicami.
 
-let a = 10, b = 20, c;
+let a = 10,
+  b = 20,
+  c;
 
 // Proste działania;
 c = a + b;
 c = a - b;
 c = a * b;
-c =  a / b; // w przypadku dzielenia przez zero zwróci Infinity
-c == a ** b // a do potegi b
+c = a / b; // w przypadku dzielenia przez zero zwróci Infinity
+c == a ** b; // a do potegi b
 
 c = a % b; // Reszta z dzielenia
 c = Math.pow(a, b); // Potęgowanie, a do potęgi b
 c = Math.sqrt(a); // Pierwiastek kwadratowy z a
-c = Math.round(a) // Zaokragla do liczby calkowitej zgodznie z matekatyka
-c = Math.ceil(a) // Zaokragla liczbe zawsze go dóry (5.3 -> 6)
-c = Math.floor(a) // Zaokrągla liczne zawsze do dolu (5.7 -> 5)
+c = Math.round(a); // Zaokragla do liczby calkowitej zgodznie z matekatyka
+c = Math.ceil(a); // Zaokragla liczbe zawsze go dóry (5.3 -> 6)
+c = Math.floor(a); // Zaokrągla liczne zawsze do dolu (5.7 -> 5)
 c = Math.abs(a); // Wartość bezwględna z a
+c = Math.trunc(a); // usuwa liczby po przecinku
 
 c = Math.PI; // Liczba Pi
-c = Math.random() //Losuje liczbe zmiennoprzecinkową od 0 do 1
+c = Math.random(); //Losuje liczbe zmiennoprzecinkową od 0 do 1
 
-
-c= a += b; // Dodaj a do b i zapisz to w a
+c = a += b; // Dodaj a do b i zapisz to w a
 c = a++; // Dodaj 1 do a (post)
-c = ++a; // Dodaj 1 do a (pre)  
+c = ++a; // Dodaj 1 do a (pre)
 
 // Zamiast rożnych rodzajów nawiasów są tylko zwykłe
-c = ((2 + 2) * 2) - 2;
+c = (2 + 2) * 2 - 2;
 
 //porównanie bez patrzenia na typy zmiennych (niezalecane)
 c = a == b; //a jest równe b. np 5 === "5" zwróci true
 c = a >= b; //a jest większe bądź równe b np 5 !== "5" zwroic false
 
 //porownanie z uwzglednieniem typo zmiennych (zalecane)
-c = a === b // np 5 === "5" zwróci false
-c = a !== b // np 5 !== "5" zwro  ci true
+c = a === b; // np 5 === "5" zwróci false
+c = a !== b; // np 5 !== "5" zwro  ci true
 
 c = !false; // '!' działa jako negacja tzn. odwrócenie.
 c = 1 == 1 && 2 == 2; // && działa jako i
@@ -43,7 +45,7 @@ c = 1 == 2 || 1 == 3; // || działa jako lub
 //Data
 const date = new Date(); // pobranie aktualnej daty
 const date2 = new Date(1); // pobranie daty 01.01.1970 + 1msc
-const date3 = new Date(1995,4,21,19,30,1); // pobranie daty rok, miesiac (liczony od 0!), dzien, godzina minuta, sekunda
+const date3 = new Date(1995, 4, 21, 19, 30, 1); // pobranie daty rok, miesiac (liczony od 0!), dzien, godzina minuta, sekunda
 
 const dzien = date.getDate(); //pobiera dzien
 const miesiac = date.getMonth(); // pobiera miesiac (liczy od 0 do 11!)
