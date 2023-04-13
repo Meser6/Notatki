d82ba4383b9698e2bd0cc06f55c49efc4770b862
Meser6 <j.kaczmarczyk95@gmail.com>
2023-04-13 22:18:16 +0200

    pigGame v1

--- Zadania/JS/KursUdemyJS/EventsAndDOM/pigGame/script.js
+++ Zadania/JS/KursUdemyJS/EventsAndDOM/pigGame/script.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const playerXScore = (number) => document.querySelector(`#score--${number}`);
+const playerScore = (number) => document.querySelector(`#score--${number}`);
 const playerSection = (number) =>
   document.querySelector(`section.player--${number}`);
 const currentScore = (number) => document.querySelector(`#current--${number}`);
 const diceImg = document.querySelector(".dice");
 const roolDiceButton = document.querySelector(".btn--roll");
 const holdButton = document.querySelector(".btn--hold");
+const newGameButton = document.querySelector(".btn--new");
 
 const score = {
   player1: 0,
@@ -21,30 +22,77 @@ function playerNumberRound() {
 }
 
 function setScore(playerNumberRound) {
-  playerXScore(playerNumberRound).textContent =
+  score[`player${playerNumberRound}`] += pointsAtCurrentRound;
+  playerScore(playerNumberRound).textContent =
     score[`player${playerNumberRound}`];
 }
 
-function roolDice(playerNumberRound) {
+function roolDice() {
   let random = Math.trunc(Math.random() * 6 + 1);
   diceImg.src = `./src/dice-${random}.png`;
-  score[`player${playerNumberRound}`] += random;
-  return random;
+  if (random == 1) {
+    currentScore(playerNumberRound()).textContent = 0;
+    pointsAtCurrentRound = 0;
+    isPlayerOneRound = !isPlayerOneRound;
+    let notPlayerNumberRound = isPlayerOneRound ? 2 : 1;
+    playerSection(playerNumberRound()).classList.add("player--active");
+    playerSection(notPlayerNumberRound).classList.remove("player--active");
+  } else {
+    pointsAtCurrentRound += random;
+  }
 }
 
 function roundLogic() {
-  pointsAtCurrentRound += roolDice(playerNumberRound());
-  setScore(playerNumberRound());
+  roolDice();
   currentScore(playerNumberRound()).textContent = pointsAtCurrentRound;
 }
 
 function holdLogic() {
+  setScore(playerNumberRound());
   isPlayerOneRound = !isPlayerOneRound;
   let notPlayerNumberRound = isPlayerOneRound ? 2 : 1;
   playerSection(playerNumberRound()).classList.add("player--active");
   playerSection(notPlayerNumberRound).classList.remove("player--active");
+  checkWin();
+  resetCurrentScore();
+}
+
+function resetCurrentScore() {
+  currentScore(1).textContent = 0;
+  currentScore(2).textContent = 0;
   pointsAtCurrentRound = 0;
+}
+
+function resetScore() {
+  playerScore(1).textContent = 0;
+  playerScore(2).textContent = 0;
+  playerSection(1).classList.add("player--active");
+  playerSection(2).classList.remove("player--active");
+  (score.player1 = 0), (score.player2 = 0);
+}
+
+function checkWin() {
+  if (score[`player1`] >= 10) {
+    roolDiceButton.remove();
+    holdButton.remove();
+    playerSection(1).classList.add("player--winner");
+  } else if (score[`player2`] >= 10) {
+    roolDiceButton.remove();
+    holdButton.remove();
+    playerSection(2).classList.add("player--winner");
+  }
+}
+
+function newGameLogic() {
+  isPlayerOneRound = true;
+  pointsAtCurrentRound = 0;
+  resetScore();
+  document.body.appendChild(roolDiceButton);
+  document.body.appendChild(holdButton);
+  playerSection(1).classList.remove("player--winner");
+  playerSection(2).classList.remove("player--winner");
 }
 
 roolDiceButton.addEventListener("click", roundLogic);
 holdButton.addEventListener("click", holdLogic);
+newGameButton.addEventListener("click", newGameLogic);
